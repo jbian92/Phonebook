88985d47324737d5b1feffc9bc0713345070124e
Julia Bian <45663604+jbian92@users.noreply.github.com>
2021-08-03 15:59:06 +0000

    add new, list, and clear options

--- phonebook.sh
+++ phonebook.sh
@@ -1,30 +1,53 @@
+# use the bash interpreter to execute the file
 #!/bin/bash
 
 PHONEBOOK_ENTRIES="bash_phonebook_entries"
 
+declare -A options=( 
+  ["new <name> <number>"]="add entry" 
+  ["list"]="display entries" 
+  ["lookup <name>"]="display number(s) associated with given name" 
+  ["remove <name>"]="delete all entries associated with given name" 
+  ["clear"]="delete entire phonebook" 
+)
 
 if [ "$#" -lt 1 ]; then
-    exit 1
+  exit 1
 
 elif [ "$1" = "new" ]; then
-    # YOUR CODE HERE #
+  # add entry to phonebook
+  # format: firstname lastname number
+  echo "$2 $3 $4" >> $PHONEBOOK_ENTRIES
 
 elif [ "$1" = "list" ]; then
-    if [ ! -e $PHONEBOOK_ENTRIES ] || [ ! -s $PHONEBOOK_ENTRIES ]; then
-        echo "phonebook is empty"
-    else
-        # YOUR CODE HERE #
-    fi
+  if [ ! -e $PHONEBOOK_ENTRIES ] || [ ! -s $PHONEBOOK_ENTRIES ]; then
+    echo "phonebook is empty"
+  else
+    # display every entry in the phonebook
+    while read line; do
+      echo "$line"
+    done < $PHONEBOOK_ENTRIES
+  fi
 
 elif [ "$1" = "lookup" ]; then
-    # YOUR CODE HERE #
+  # display all phone numbers associated with given name
+  while read line; do
+    echo
+  done < $PHONEBOOK_ENTRIES
 
 elif [ "$1" = "remove" ]; then
-    # YOUR CODE HERE #
+  # delete all entries associated with given name
+  echo "remove"
 
 elif [ "$1" = "clear" ]; then
-    # YOUR CODE HERE #
+  # delete the entire phonebook
+  rm $PHONEBOOK_ENTRIES
 
 else
-     # YOUR CODE HERE #
+  # display valid commands
+  echo "Please enter a valid command:"
+  for option in "${!options[@]}"; do
+    echo "$option - ${options[$option]}"
+  done
+
 fi
